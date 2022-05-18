958ac8f2696485a16a6a5430794febb26c68f407
mike <mike_wiii@hotmail.com>
2022-05-18 05:29:20 -0500

    assets added and index

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$('document').ready(function () {
+    $('#preview > button').hide();
+    $('body').fadeIn(3000);
+
+    //const menu = new Menu($('#character'), '../assets/character/meshes/Boxing.fbx', '');
+
+
+    $('.modes-list').hover(function () {
+        /* const soundfx = new Audio('../soundfx/select-tiem.wav');
+        soundfx.play(); */
+        switch (this.getAttribute('mode')) {
+            case 'arcade':
+                $('#character div > p').text('This game mode is for those who want to have a good time, defeat their greatest enemies one after another to win the title of world champion, the most courageous fighter of all time.');
+                break;
+
+            case 'versus':
+                $('#character div > p').text('FIGHT FACE TO FACE WITH YOUR HIGEST RIVAL, CRUSH HIM AND MAKE BITE THE DUST OF YOUR FIST, SHOW HIM HOW BLUE STEEL FEEL.');
+		$('#preview > button').hide();
+                break;
+
+            case 'traning':
+                $('#character div > p').text('GET BETTER AND BETTER, IMPROVE YOUR MOVES, BECOME INTO THE HIGEST FIGHTER OF ALL TIMES TO WIN A WORLD CUP. TIME AND PRACTICE MAKE MASTERS');
+		$('#preview > button').hide();
+                break;
+
+            case 'online':
+                $('#character div > p').text('IMPROVE Y0UR SELF WITH PLAYERS AROUND THE HOLE WORLD, BEAT THEM AND BE THE ONE ON WORLD WIDE SCORE CHART, WHEN YOU GET THERE, YOU WILL BE A REAL CHAMPIO');
+		$('#preview > button').hide();
+                break;
+
+            case 'session':
+                $('#character div > p').text('LOGIN OR SIGN IN FOR AN ACCOUNT AND GET YOU INTO WORLD WIDE SCORE CHART');
+		$('#preview > button').show();
+                break;
+
+		case 'start':
+                $('#character div > p').text('LOGIN OR SIGN IN FOR AN ACCOUNT AND GET YOU INTO WORLD WIDE SCORE CHART');
+		$('#preview > button').show();
+                break;
+
+
+            case 'settings':
+                $('#character div > p').text('CHANGE MUSIC AND SCREEN SETTINGS');
+		$('#preview > button').hide();
+                break;
+
+            default:
+                break;
+        }
+    })
+
+})
--- index.php
+++ index.php
@@ -1,142 +1,175 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <!-- GOOGL FONTS API -->
-    	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
 
-    <title>FIGHTER JAM | MENU</title>
-  </head>
-  <body class="m-0 p-0">
-	
-	<div class="container-expand-lg bg-primary d-none">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+		<link rel="stylesheet" href="<?php echo 'http://localhost/figterjam/assets/css/index.css' ?>">
+	<title>Fighter Jam | Animal Style</title>
+
+</head>
+
+<body>
+	<div class="container-expand-lg">
 		<div class="col-12">
 			<div class="m-5 row">
 				<h1 class="text-white text-center title">
 					FIGHTER JAM
-					<small> ANIMAL STYLE </small>
+					<small>ANIMAL STYLE</small>
 				</h1>
 			</div>
 			<div class="row">
-				<h2 class="text-white text-center"> MENU MODE </h2>
+				<h2 class="text-white text-center">MENU MODE</h2>
 			</div>
-			<div id="menu">
+			<div id="menu" data-user="<?php echo $user != null ? $user->serializeJSON() : ""; ?>" class="m-5 row">
 				<div class="col-3">
+					<!-- ITEMS RENDERED BY JAVASCRIPT -->	
+				</div>
+				<div id="character" class="col-9">
+					<div class="col-bg-primary">
+						<div class="row">
+							<p class="fs-3 fw-bold text-center">
+							</p>
+						</div>
+						<div class="row" id="preview">
+							<!-- GAME MODE IMAGE PREVIEW -->
+							<!-- Button trigger modal -->
+							<button type="button" style="font-family: 'Shadows Into Light', cursive" class="text-white fs-bold fs-2 btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+								LOGIN
+							</button>
+
+
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-    	<script>
-		
-		$('document').ready(()=>{
-			let session;
-			$.ajax({
-				url:'http://23.22.80.119/figterjam/menu.php',
-				data: {},
-				success: function(res){
-						console.log(JSON.parse(res));
-						document.querySelector('#loading').remove();
-						document.querySelector('.container-expand-lg').classList.remove('d-none')
-					}
-				});
+	<!-- Modal -->
+	<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+		aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					...
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Understood</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
-			const menu = document.querySelector('#menu');
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="../libs/three.min.js"></script>
+	<!-- <script src="../libs/three.min.js"></script>
+	<script src="../libs/FBXLoader.js"></script>
+	<script src="../libs/game.js"></script>
+	<script src="../libs/inflate.min.js"></script>
+	<script src="../libs/game.js"></script> -->
+<script src="<?php echo 'http://localhost/figterjam/assets/js/index.js' ?>"></script>
 
-		});	
+<script>
+	const menuContainer = document.querySelector('#menu > div.col-3');
+	const menu = document.querySelector('#menu');
+	//RENDER LIST ITEMS BY MODULES
+	const ULNode =  () =>{
+		const ul = document.createElement('ul');
+		ul.className = 'list-group';
 
-		const loading = ()=>{
+		return ul;
+	}
 
-			const container = document.createElement('div');
-			container.id = 'loading';
-			container.className = 'container-expand-lg d-flex align-items-center vh-100';
+	const ItemsNodes = (item)=>{
+		const li = document.createElement('li');
+		li.className = 'modes-list fs-1 list-group-item'; 
 
-			const spinnerContainer = document.createElement('div');
-			spinnerContainer.className = 'spinner-border m-5 mx-auto';
-			spinnerContainer.setAttribute('role', 'status');
+		//SET ATRIBUTES
+		li.setAttribute('mode', item.mode);
 
-			container.append(spinnerContainer);
+		//SET ANCHORE CONTENT
+		const anchore = document.createElement('a');
+		anchore.href = item.href;
+		anchore.textContent = item.text;
 
-			return container;
+		//APPEND
+		li.append(anchore);
 
+		return li;
+
+	}
+	
+	const modes = [
+		{
+			mode: 'arcade',
+			href: 'arcade.html',
+			text: 'ARCADE MODE'
+		},
+		{ 
+			mode: 'versus',
+			href: '#',
+			text: 'VERSUS MODE'
+		},
+		{ 
+			mode: 'traning',
+			href: '#',
+			text: 'TRANING MODE'
+		},
+		{ 
+			mode: 'settings',
+			href: '#',
+			text: 'SETTINGS'
+		},
+		{ 
+			mode: 'session',
+			href: '#',
+			text: 'LOGIN'
+		},
+		{ 
+			mode: 'start',
+			href: '#',
+			text: 'SIGN IN' 
 		}
 
-		const ULNode = ()=>{
-			const ul = document.createElemet('ul');
-			ul.className = 'list-grup';
+	];
 
-			return ul;	
-		}
+	if(menu.dataset.user != ''){
+		modes.pop();
+		document.querySelector('button').remove();
+	}
 
-		const ItemNode = (item)=>{
-			const li = document.createElement('li');
-			li.className = 'modes-list fs-1 list-group-item';
+	let ul = ULNode();
+	const items = modes.map(ItemsNodes);
 
-			li.setAttribute('mode', item.node);
+	ul.append(...items);
+	menuContainer.append(ul);
 
-			const anchore = document.createElement('a');
-			anchore.href = item.href;
-			anchore.textContent = item.text;
+</script>
 
-			li.append(anchore);
+</body>
 
-			return li;
-		}
-
-		const modes = [
-			{
-				mode: 'arcade',
-				href: 'arcade.html',
-				text: 'ARCADE MODE'
-			},
-			{ 
-				mode: 'versus',
-				href: '#',
-				text: 'VERSUS MODE'
-			},
-			{ 
-				mode: 'traning',
-				href: '#',
-				text: 'TRANING MODE'
-			},
-			{ 
-				mode: 'settings',
-				href: '#',
-				text: 'SETTINGS'
-			},
-			{ 
-				mode: 'session',
-				href: '#',
-				text: 'LOGIN'
-			},
-			{ 
-				mode: 'start',
-				href: '#',
-				text: 'SIGN IN' 
-			}
-
-		];
-
-
-		document.body.append(loading());
-	</script>
-  </body>
 </html>
-
